92e0683f57cbd3e0130c3c80a5cd294330b9a8be
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-10-26 15:18:56 +0500

    add buttons

---
+++ defoult_buttons.py
@@ -0,0 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+btn = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+
+b1 = KeyboardButton(text="🚀Kurslarga yozilish / to'liq ma'lumot olish")
+b2 = KeyboardButton(text='🏢Bizning joylashuv')
+b3 = KeyboardButton(text='📞Admin bilan aloqa')
+
+
+btn.add(b1, b2, b3)
+
+contact = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+c2 = KeyboardButton(text="Telefon raqmni ulashish", request_contact=True)
+contact.add(c2)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from aiogram import Bot, types, executor, Dispatcher
 import logging
-
 from config import TOKEN
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+
+from defoult_buttons import btn, contact
+from states import Reg
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,8 +18,41 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands="start")
 async def start(msg: types.Message):
     text = f"Assalomu alekum {msg.from_user.full_name} bizning botimizga hush kelibsiz!"
-    await msg.answer(text)
+    await msg.answer(text, reply_markup=btn)
 
+
+@dp.message_handler(text='Registratsiya')
+async def first_name(msg: types.Message):
+    text = "Ismingizni kiriting: "
+    await msg.answer(text)
+    await Reg.first_name.set()
+
+
+@dp.message_handler(state=Reg.first_name)
+async def name(msg: types.Message, state: FSMContext):
+    await state.update_data(first_name=msg.text)
+    text = "Yoshingizni kiriting: "
+    await msg.answer(text)
+    await Reg.age.set()
+
+
+@dp.message_handler(state=Reg.age)
+async def age(msg: types.Message, state: FSMContext):
+    await state.update_data(age=msg.text)
+    text = "Telfon raqamingizni kiriting: "
+    await msg.answer(text, reply_markup=contact)
+    await Reg.phone_number.set()
+
+
+@dp.message_handler(state=Reg.phone_number, content_types=types.ContentTypes.CONTACT)
+async def phone_number(msg: types.Message, state: FSMContext):
+    await state.update_data(phone_number=msg.contact.phone_number)
+    s = await state.get_data()
+    text = f" Ismingiz: {s.get('first_name')}\n" \
+           f"Yoshingiz: {s.get('age')}\n" \
+           f"Telefon raqamingiz: {s.get('phone_number')}"
+    await msg.answer(text)
+    await state.finish()
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
